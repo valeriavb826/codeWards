d4417f268ad3a7449cc06b99e2614eec141d09ce
VALERIA VILLALOBOS <100798630+valeriavb826@users.noreply.github.com>
2022-04-08 12:07:03 -0500

    DNA to RNA conversion description

--- abbreviate a two word name/app.js
+++ abbreviate a two word name/app.js
@@ -21,3 +21,13 @@
 //     return firstInitial + '.' + secondInitial
 
 // }
+
+
+
+
+// other solution
+// function abbrevName(name){
+
+//     var nameArray = name.split(" ");
+//     return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+//   }
