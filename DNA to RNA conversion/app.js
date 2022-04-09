5d579fa90a70fb95f382bcdbd182a46eb88ba17e
VALERIA VILLALOBOS <100798630+valeriavb826@users.noreply.github.com>
2022-04-09 12:15:05 -0500

    DNA to RNA conversion description

--- DNA to RNA conversion/app.js
+++ DNA to RNA conversion/app.js
@@ -10,3 +10,11 @@
 
 // "GCAT"  =>  "GCAU"
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+
+
+
+// My solution
+// function DNAtoRNA(dna){
+//     return dna.replace(/T/g, "U");
+//   }
