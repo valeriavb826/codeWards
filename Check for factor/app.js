e7302e54553911fa21e1bab7f8a1b35edc2b86c1
VALERIA VILLALOBOS <100798630+valeriavb826@users.noreply.github.com>
2022-04-12 13:59:57 -0500

    check for factor description

--- Check for factor/app.js
+++ Check for factor/app.js
@@ -13,4 +13,3 @@
 // You can use the mod operator (%) in most languages to check for a remainder
 // For example 2 is not a factor of 7 because: 7 % 2 = 1
 
-// Note: base is a non-negative number, factor is a positive number.
