8a2d28479bf7bf72be2a56b4dd337757383a0f67
VALERIA VILLALOBOS <100798630+valeriavb826@users.noreply.github.com>
2024-02-05 18:22:08 -0600

    my answer

---
+++ _Sum without highest and lowest number/app.js
@@ -0,0 +1,7 @@
+// Task 
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+
+sumArr = array = array.sort((a,b) => a-b).slice(1,-1).reduce((total,num)=> total+num)
